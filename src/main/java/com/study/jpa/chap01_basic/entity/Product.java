8bdb5b79c7571264b48aabe6ea6a4021493e1319
yangwoo <kyy77321@naver.com>
2023-11-20 13:06:58 +0900

    #1. 엔터티 클래스 생성과 기본 CRUD

--- src/main/java/com/study/jpa/chap01_basic/entity/Product.java
+++ src/main/java/com/study/jpa/chap01_basic/entity/Product.java
@@ -1,12 +1,47 @@
 package com.study.jpa.chap01_basic.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
+@Table(name = "tbl_product")
 public class Product {
 
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "prod_id") // 필드명과 컬럼명을 다르게 하고 싶을때
+    private long id;
+
+    @Column(name = "prod_name", nullable = false, unique = true, length = 30) // nullable = false not null unique = true 중복 안됨
     private String name;
+
+    private int price;
+
+    @Enumerated(EnumType.STRING)
+    // enumType을 ordinal로 하면 Category를 int형태로 받아온다 기본값
+    @Column(length = 6000)
+    private Category category;
+
+    @CreationTimestamp // default: current_timestamp(오라클의 sysdate)
+    @Column(updatable = false) // update시 쿼리가 돌아가지 않게함
+    private LocalDateTime createDate;
+
+    @UpdateTimestamp // 업데이트시 자동으로 날짜 변경
+    private LocalDateTime updateDate;
+
+    public enum Category {
+        FOOD, FASHION, ELECTRONIC
+    }
+
 }
